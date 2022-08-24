cc09190cc426df5e748fd9d4219ce416f0f0d856
VitaliiFedan <vitalii@herdwatch.com>
2022-08-24 14:18:36 +0300

    add class UserMapper

---
+++ src/main/java/com/foxminded/vitaliifedan/task10/mappers/UserMapper.java
@@ -0,0 +1,19 @@
+package com.foxminded.vitaliifedan.task10.mappers;
+
+import com.foxminded.vitaliifedan.task10.models.persons.*;
+
+public class UserMapper {
+
+    private UserMapper() {
+    }
+
+    public static User createUser(String name, String surname, String login, String password, Roles role, UserTypes userType) {
+        if ("TEACHER".equals(userType.toString())) {
+            return new Teacher(name, surname, login, password, role, userType);
+        } else if ("STUDENT".equals(userType.toString())) {
+            return new Student(name, surname, login, password, role, userType);
+        } else {
+            return new User(name, surname, login, password, role, userType);
+        }
+    }
+}
