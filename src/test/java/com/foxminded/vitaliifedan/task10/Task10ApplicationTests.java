9d89866eddfa78ee934173a4d30912080983ee7e
VitaliiFedan <vitalii@herdwatch.com>
2022-08-24 14:27:55 +0300

    other files

---
+++ src/test/java/com/foxminded/vitaliifedan/task10/Task10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxminded.vitaliifedan.task10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
