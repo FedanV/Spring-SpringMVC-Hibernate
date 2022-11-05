83fdfd3a72c1c0224bd30e5987f8fce3ecca8c5e
VitaliiFedan <vitalii@herdwatch.com>
2022-11-05 16:15:24 +0200

    remove dao

--- src/main/java/com/foxminded/vitaliifedan/task10/dao/LectureDao.java
+++
@@ -1,6 +0,0 @@
-package com.foxminded.vitaliifedan.task10.dao;
-
-import com.foxminded.vitaliifedan.task10.models.schedules.Lecture;
-
-public interface LectureDao extends CrudDao<Lecture, Integer> {
-}
